5833faf9a2c261ba167b3b644d8e74d37fe27fc6
Gustavo Carvalho <gustavo@externalsecrets.com>
2025-07-16 18:44:35 -0300

    fix: debug
    Signed-off-by: Gustavo Carvalho <gustavo@externalsecrets.com>

--- services/secrets/secrets.go
+++ services/secrets/secrets.go
@@ -25,10 +25,14 @@ func (s *Service) UpdateVersion(newValue []byte, entry *models.ScanEntry) error 
 		return err
 	}
 	newFileLength := len(file) - (entry.EndLine - entry.StartLine + 1) + len(newValue)
+	fmt.Printf("old file length: %v\nold Entry Len: %v\n, new Entry Len: %v\n, new file len: %v\n", len(file), entry.EndLine-entry.StartLine+1, len(newValue), newFileLength)
 	modFile := make([]byte, newFileLength)
 	fmt.Printf("modifying %v with %v", string(file[entry.StartLine:entry.EndLine]), string(newValue))
 	copy(modFile[:entry.StartLine], file[:entry.StartLine])
+	fmt.Printf("New modFile before start: %v\n", string(modFile[:entry.StartLine]))
 	copy(modFile[entry.StartLine:entry.StartLine+len(newValue)], newValue)
+	fmt.Printf("New modFile up until beginning: %v\n", string(modFile[:entry.StartLine+len(newValue)]))
 	copy(modFile[entry.StartLine+len(newValue):], file[entry.EndLine:])
+	fmt.Printf("New modFile after end: %v\n", string(modFile))
 	return os.WriteFile(entry.FilePath, modFile, 0644)
 }
